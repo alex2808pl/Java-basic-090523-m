0adfbcf64a5dbbe354af9f316ccd373c33fd87d0
Aleksandr Plakushchyi <alex2808pl@gmail.com>
2023-08-08 12:54:27 +0300

    lesson24-2

--- src/main/java/de/telran/lesson24/practice2/Calculator.java
+++ src/main/java/de/telran/lesson24/practice2/Calculator.java
@@ -1,5 +1,6 @@
-package de.telran.lesson24.practice1;
+package de.telran.lesson24.practice2;
 
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,12 +13,15 @@ public class Calculator {
     public static final char REST_SIGN = '%';
     public static final String ANSWER_YES = "y";
     public static final String ANSWER_NO = "n";
+    public static final int HISTORY_SIZE = 3;
+
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.ENGLISH);
 
         String needNextCalculation;
+        String[] resultsHistory = new String[HISTORY_SIZE]; // Массив для запоминания истории 5 последних вычислений
         do {
 
             int argumentsNumber;
@@ -26,30 +30,30 @@ public class Calculator {
                 argumentsNumber = scanner.nextInt();
             } while (argumentsNumber < 2);
 
-            double[] arguments = new double[argumentsNumber];
+            double[] arguments = new double[argumentsNumber]; // Массив для хранения аргументов
+            char[] signs = new char[argumentsNumber - 1]; // Массив для хранения знаков математических операций
+
             for(int i = 0; i < arguments.length; i++) {
                 System.out.println(String.format("Введите значение %d аргумента (разделитель целой и дробной части - точка):", i+1));
                 arguments[i] = scanner.nextDouble();
+                if (i < arguments.length - 1) { // Ввод знаков математической операции для аргументов с первого до предпоследнего
+                    do {
+                        System.out.print("Введите знак математической операции (+, -, *, /): ");
+                        signs[i] = scanner.next().charAt(0); // Сохранение введенного знака в массив знаков операций
+                    } while (signs[i] != PLUS_SIGN && signs[i] != MINUS_SIGN && signs[i] != MULTIPLY_SIGN
+                            && signs[i] != DIVIDE_SIGN && signs[i] != REST_SIGN);
+                }
             }
-
-            char sign;
-            do {
-                System.out.println("Введите знак математической операции (+, -, *, /, %):");
-                String signString = scanner.next();
-                sign = signString.charAt(0);
-            } while (sign != PLUS_SIGN && sign != MINUS_SIGN && sign != MULTIPLY_SIGN && sign != DIVIDE_SIGN && sign != REST_SIGN);
-
-// Это наш технический долг !!! :)
+            // Это наш технический долг !!! :)
 //            double argumentTwo;
 //            do {
 //                System.out.println("Введите значение второго аргумента (разделитель целой и дробной части - точка):");
 //                argumentTwo = scanner.nextDouble();
 //            } while (argumentTwo == 0 && (sign == DIVIDE_SIGN || sign == REST_SIGN));
 
-
             double result = arguments[0];
             for(int i = 1; i<arguments.length; i++) {
-                switch (sign) { // Выполнение математической операции в зависимости от введенного знака
+                switch (signs[i - 1]) { // Выполнение математической операции в зависимости от введенного знака
                     case PLUS_SIGN: // Если пользователь ввёл знак + ...
                         result += arguments[i]; // ... выполняется операция сложения
                         break;
@@ -70,11 +74,19 @@ public class Calculator {
             StringBuilder resultMessage = new StringBuilder();
             for (int i=0; i<arguments.length; i++) {
                 resultMessage.append(String.format("%.2f",arguments[i]));
-                if(i < arguments.length-1) resultMessage.append(sign);
+                if(i < arguments.length-1) resultMessage.append(signs[i]);
             }
             resultMessage.append("=").append(String.format("%.2f",result));
 
-            System.out.println(resultMessage); //вывожу на экран результат
+            // Освобождаем первую ячейку массива для сохранения последнего результата работы программы, сдвигая значения на 1 ячейку вперёд, начиная с конца
+            for (int i = resultsHistory.length - 1; i > 0; i--) {
+                resultsHistory[i] = resultsHistory[i - 1];
+            }
+            resultsHistory[0] = resultMessage.toString(); //заносим в историю результат текущего выражения
+
+            System.out.println("История выполненных вычислений:");
+            System.out.println(Arrays.toString(resultsHistory)); //вывожу на экран историю результатов
+            System.out.println();
 
             do {
                 System.out.println("Выполнить еще одно вычисление? (Y/N):");
@@ -89,12 +101,12 @@ public class Calculator {
 
 
  /*
-1) Обновите функционал до производящего вычисления (+, -, *, /) с произвольным количеством чисел
-c дробной частью.
-2) Количество аргументов пользователь вводит с клавиатуры. Оно должно быть от 2 и более.
-3) Тип данных вводимых аргументов должен быть Double.
-4) Далее пользователь последовательно вводит значение каждого из аргументов, а затем - знак математической
-операции, которую необходимо выполнить со значениями введенных аргументов.
-5) Результат работы программы необходимо вывести в виде вычисляемого выражения и полученного результата
-с точностью до двух чисел после запятой.
+1) Вводить знак математической операции перед вводом каждого следующего аргумента.
+   Например, пользователь указывает, что хочет выполнить операцию над тремя аргументами.
+   Программа просит последовательно ввести первый аргумент, знак математической операции,
+     который будет выполнен над значениями первого и второго аргумента, значение второго аргумента,
+     знак математической операции, который будет выполнен над результатом операции между первым и
+     вторым аргументом и третьим аргументом.
+     В результате, программа должна позволять вычислить следующее выражение: 3 * 4 - 10.5 + 2 = 3.5
+2) Выводить на экран историю последних 3 вычислений.
 */
